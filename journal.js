cf7a96e970d6d2fe5f43be46bc47c9c9a41b2b3f
EtienneT64 <110019770+EtienneT64@users.noreply.github.com>
2023-02-28 21:53:42 +0200

    created journal.js and added logActivity function

---
+++ journal.js
@@ -0,0 +1,7 @@
+// prototype function //TODO add automatic date incrementing
+function logActivity(date, activity) {
+	msg = `On:${date}, I did: ${activity}`;
+	console.log(msg);
+}
+
+logActivity('2023/02/28','Learnt and coded many functions');
